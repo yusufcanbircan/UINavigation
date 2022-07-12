6ed15f2f9a606b73cf0c94159b487ca8bc5a9f73
Yusuf Can Bircan <yusufcannnz@hotmail.com>
2022-07-12 22:28:45 +0300

    Initial Commit

---
+++ UITableView/Controller/InitialTableViewController.swift
@@ -0,0 +1,114 @@
+//
+//  InitialTableViewController.swift
+//  UITableView
+//
+//  Created by Yusuf Can on 12.07.2022.
+//
+
+import UIKit
+
+class InitialTableViewController: UITableViewController {
+
+    var tableViewData = [
+        ["General", "Accessibility", "Privacy"],
+        ["Password"],
+        ["Safari", "News", "Translate", "Maps", "Shortcuts", "Health", "Siri & Search", "Photos"],
+        ["Developer"]
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return tableViewData.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return tableViewData[section].count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = tableViewData[indexPath.section][indexPath.row]
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let alertController = UIAlertController(title: "Alert",
+                                                message: tableViewData[indexPath.section][indexPath.row],
+                                                preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK",
+                                                style:UIAlertAction.Style.default,
+                                                handler: {_ in
+            alertController.dismiss(animated: false)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Delete",
+                                                style: .destructive,
+                                                handler: {_ in
+            self.tableViewData[indexPath.section].remove(at: indexPath.row)
+            tableView.reloadData()
+        }))
+        
+        present(alertController, animated: true)
+        
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
